b7515bec3bed6cf6a5937aff02f10388dabcd956
WuKe <118019715+source-code-examples@users.noreply.github.com>
2026-08-11 17:55:52 +0200

    feat: add basic ReservationList component

---
+++ client/src/components/ReservationList.tsx
@@ -0,0 +1,26 @@
+function ReservationList() {
+  return (
+    <section className="reservation-list">
+      <h2>Reservations</h2>
+
+      <ul>
+        <li>
+          <div>
+            <strong></strong>
+            <span></span>
+          </div>
+
+          <div></div>
+
+          <div></div>
+
+          <button type="button" className="delete-btn">
+            Delete
+          </button>
+        </li>
+      </ul>
+    </section>
+  );
+}
+
+export default ReservationList;
